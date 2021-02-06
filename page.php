9a1c1b3d140281af9ab4aeb5f4b6a20bc5c82918
brian <thisnameistak#n11@gmail.com>
2021-02-06 13:57:24 -0700

    basic page chages -- setup for animation trn

--- page.php
+++ page.php
@@ -2,10 +2,18 @@
 get_header();
 ?>
 <div class="page"  data-barba="container" data-barba-namespace="recipes">
-    <div class="hero__wrapper" >
-        <h1>page.php</h1>
+    <div class="page__wrapper" >
+        <h1><?php echo wp_get_post_parent_id( get_the_ID() ) ?></h1>
         <?php
             while(have_posts()){
+                $is_parant = wp_get_post_parent_id( get_the_ID() );
+                if($is_parant != 0){
+                    echo "this is not a parant page";
+                    
+                }else{
+                    echo "this is a parent page";
+                }
+                
                 the_post();
                 echo the_title();
                 echo the_content();
