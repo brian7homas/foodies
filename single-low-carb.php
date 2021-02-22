4962a2b23cfaea574a86c9a6da91a6746f4c10ed
brian <thisnameistak#n11@gmail.com>
2021-02-22 11:20:16 -0700

    Refactored ingrediant custom field, added styles for single page

--- single-low-carb.php
+++ single-low-carb.php
@@ -8,27 +8,26 @@
         <div class="single-recipe__img" style="background-image: linear-gradient(to left bottom, rgba(27, 67,50, .2), rgba(82, 183, 136, 1)),url('<?php echo get_field('image');?>');">
             <div class="single-recipe__content">
                     <h1 class="single-recipe--title"><?php the_title();?></h1>
-                    <div class="single-recipe--facts__3">
-                            <p>Type: <?php echo get_field('type');?></p>
-                            <h3>Carbs:<?php echo get_field('carbohydrates');?></h3>
-                            <h3>Calories: <?php echo get_field('calories');?></h3>
+                    <div class="single-recipe__container flex-container">
+                        <div class="single-recipe--facts__3">
+                                <p>Type: <?php echo get_field('type');?></p>
+                                <h3>Carbs:<?php echo get_field('carbohydrates');?></h3>
+                                <h3>Calories: <?php echo get_field('calories');?></h3>
+                        </div>
+                        <div class="single--ingrediants">
+                            <?php echo get_field('ingrediants');?>
+                        </div>            
                     </div>
             </div>
+        </div>
+        <div>
+            <a href="<?php echo get_post_type_archive_link('low-carb'); ?>"> 
+                Go back
+            </a>
         </div>
         
         <div id="<?php echo the_ID();?>" class="single-recipe__information" >
             <div class="single-recipe__information--item">
-            
-                <div class="single--ingrediants">
-                    <h2 class="single-recipe__information--headline">ingrediants</h2>
-                    <!-- <ul class="single--ingrediants">
-                        <li><?php// the_content();?></li>
-                    </ul> -->
-                    
-                    <div class="single--ingrediants">
-                        <?php the_content();?>
-                    </div>
-                </div>
                 <div class="single--instructions">
                     <h2 class="single-recipe__information--headline">Instructions</h2>
                     <p>
