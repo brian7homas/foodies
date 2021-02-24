317ef0b1bdab059d7757fe913d5d9e3a3cf07f8d
brian <thisnameistak#n11@gmail.com>
2021-02-24 08:15:30 -0700

    Restructured single layout

--- single-low-carb.php
+++ single-low-carb.php
@@ -1,13 +1,14 @@
 <?php 
     get_header();
-    echo "single-low-carb";
+    
+    $img = get_field('image');
 ?>
 <div class="single-recipe"  >
     <div class="single-recipe__wrapper" data-barba="container" data-barba-namespace="archive" >
-        
-        <div class="single-recipe__img" style="background-image: linear-gradient(to left bottom, rgba(27, 67,50, .2), rgba(82, 183, 136, 1)),url('<?php echo get_field('image');?>');">
-            <div class="single-recipe__content">
-                    <h1 class="single-recipe--title"><?php the_title();?></h1>
+        <div class="single-recipe__img" style="background-image: linear-gradient(to bottom, rgba(27, 67,50, .2), rgba(82, 183, 136, 1)),url('<?php echo $img['sizes']['contentLandscape'];?>');">
+            <h1 class="single-recipe--title"><?php the_title();?></h1>
+        </div>
+        <div class="single-recipe__content">
                     <div class="single-recipe__container flex-container">
                         <div class="single-recipe--facts__3">
                                 <p>Type: <?php echo get_field('type');?></p>
@@ -19,15 +20,13 @@
                         </div>            
                     </div>
             </div>
-        </div>
-        <div>
-            <a href="<?php echo get_post_type_archive_link('low-carb'); ?>"> 
-                Go back
-            </a>
-        </div>
-        
         <div id="<?php echo the_ID();?>" class="single-recipe__information" >
             <div class="single-recipe__information--item">
+                <div class="right-align">
+                    <a href="<?php echo get_post_type_archive_link('keto'); ?>"> 
+                        Go back
+                    </a>
+                </div>
                 <div class="single--instructions">
                     <h2 class="single-recipe__information--headline">Instructions</h2>
                     <p>
