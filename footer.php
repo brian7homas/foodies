f4e321d130f4b065642d7ded36a51feaffb97f96
brian-7homas <brian.thomas2024@outlook.com>
2024-02-05 07:40:32 -0700

    refactor(Footer): LINKS
    updated footer links

--- footer.php
+++ footer.php
@@ -4,9 +4,9 @@
         <nav class="footer__nav-menu">
             <ul class="footer__nav-menu__wrapper">
                 <a href="<?php echo site_url('/')?>"><li class="footer__nav-menu__item">Home</li></a>
-                <a href="https://joyreacher.com"><li class="footer__nav-menu__item">Back to Portfolio</li></a>
+                <a href="https://brianthomas-develops.com"><li class="footer__nav-menu__item">Back to Portfolio</li></a>
             </ul>
-            <p>Wordpress App developed and designed by <a href="http://linkedin.com/in/joyreacher">Brian Thomas</a></p>
+            <p>Wordpress App developed and designed by <a href="www.linkedin.com/in/brianthomas--develops">Brian Thomas</a></p>
         </nav>
     </div>
 </footer>
